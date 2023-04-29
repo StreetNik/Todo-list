300ae0f7ac31399b87d9dfe513a12a1350c2f807
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-04-29 12:40:47 -0500

    Solution

--- task/urls.py
+++ task/urls.py
@@ -9,11 +9,11 @@ from task.views import HomePageView, CreateNewTask, UpdateTaskView,\
 
 urlpatterns = [
   path("", HomePageView.as_view(), name="home"),
-  path("create-new-task", CreateNewTask.as_view(), name="new-task"),
+  path("create-new-task/", CreateNewTask.as_view(), name="new-task"),
   path("<int:pk>/update/", UpdateTaskView.as_view(), name="update-task"),
   path("<int:pk>/delete/", DeleteTaskView.as_view(), name="delete-task"),
   path("tag/", TagListView.as_view(), name="tags"),
-  path("tag/create-new-tag", CreateNewTag.as_view(), name="new-tag"),
+  path("tag/create-new-tag/", CreateNewTag.as_view(), name="new-tag"),
   path("tag/<int:pk>/update/", UpdateTagView.as_view(), name="update-tag"),
   path("tag/<int:pk>/delete/", DeleteTagView.as_view(), name="delete-tag"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
